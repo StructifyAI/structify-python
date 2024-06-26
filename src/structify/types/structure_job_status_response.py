2a636dafd4a14347f3bea02a57cc8eb3ecd2c8c0
Matthew Habtezgi <mhbtz@mit.edu>
2024-06-26 14:14:43 -0700

    pass

--- src/structify/types/structure_job_status_response.py
+++ src/structify/types/structure_job_status_response.py
@@ -1,10 +1,9 @@
 # File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
-
-from typing import List, Tuple
+from typing import List, Union, Optional
 from typing_extensions import Literal
 
 from ..types.logger import Log
 
 __all__ = ["StructureJobStatusResponse"]
 
-StructureJobStatusResponse = Tuple[List[Literal["Running", "Completed", "Failed"]], Log]
+StructureJobStatusResponse = List[Union[Literal["Running", "Completed", "Failed"], Optional[Log]]]
