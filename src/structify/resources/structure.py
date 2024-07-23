4d813ca8d3dceff6be9aa4ba6a871c0b897829a5
Alex Reichenbach <8087473+Reichenbachian@users.noreply.github.com>
2024-07-23 10:00:01 -0700

    fixed

--- src/structify/resources/structure.py
+++ src/structify/resources/structure.py
@@ -207,7 +207,7 @@ class StructureResource(SyncAPIResource):
 
             successfully_started_job = True
             if status == "Completed":
-                return self._client.datasets.view(dataset_name=kwargs["dataset_name"], table_name=table_name, requested_type="Entities") # type: ignore
+                return self._client.datasets.view(dataset_name=kwargs["dataset_name"], table_name=table_name, requested_type="Entities")  # type: ignore
             time.sleep(1)
     # -------------------------------------------------------------------------
 
